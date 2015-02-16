fbeb7f8978e7035a6045525a4cb397e2eba5aa1e
Ameya <ameyaajay.joshi@sjsu.edu>
2015-02-15 19:32:32 -0800

    First Commit

---
+++ src/main/java/com/glassify/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.glassify.controller;
+
+import org.springframework.stereotype.Controller;
+
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+}
