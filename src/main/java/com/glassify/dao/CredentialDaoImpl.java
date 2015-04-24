cac1beaf4f59aea680a4b7b3ef5ec75f69e84947
Ameya <ameyaajay.joshi@sjsu.edu>
2015-04-23 17:24:46 -0700

    uncommenting..

--- src/main/java/com/glassify/dao/CredentialDaoImpl.java
+++ src/main/java/com/glassify/dao/CredentialDaoImpl.java
@@ -51,7 +51,7 @@ public class CredentialDaoImpl implements CredentialDao {
 						
 						Credential credential = new Credential(BearerToken.authorizationHeaderAccessMethod());
 						credential.setAccessToken(rs.getString("access_token"));
-						//credential.setRefreshToken(rs.getString("refresh_token"));
+						credential.setRefreshToken(rs.getString("refresh_token"));
 						myCredential.setCredential(credential);
 						
 						return myCredential;
